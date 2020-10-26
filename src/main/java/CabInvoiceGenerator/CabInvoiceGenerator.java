d887b2512108ebb9222384e901fd98f58e22c99f
Jagriti <jagriti.agni@gmail.com>
2020-10-26 15:14:34 +0530

    [Jagriti]ADDED: Ride class and method to find total fare of multiple rides

--- src/main/java/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -9,4 +9,12 @@ public class CabInvoiceGenerator {
 		double totalFare = (distance * MINIMUM_COST_PER_KM) + (time * MIN_COST_PER_TIME);
 		return Math.max(totalFare, MIN_TOTAL_FARE);
 	}
+
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare = totalFare + calculateFare(ride.getDistance(), ride.getTime());
+		};
+		return totalFare;
+	}
 }
