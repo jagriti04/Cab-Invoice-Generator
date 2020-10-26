38789193657c33d1ad2fbb0512a415649936410d
Jagriti <jagriti.agni@gmail.com>
2020-10-26 15:22:56 +0530

    [Jagriti]ADDED: Global variable of invoice service class

--- src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -1,34 +1,38 @@
 package CabInvoiceGenerator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+	CabInvoiceService invoiceService = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		invoiceService = new CabInvoiceService();
+	}
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		double fare = invoiceService.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
 	
 	@Test
 	public void givenLessDistanceAndTime_ShouldReturnMinFare () {
-		CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		double fare = invoiceService.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
 	}
 	
 	@Test
 	public void givenMultipleRides_ShouldReturnTotalFare () {
-		CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
 		Ride[] rides = { new Ride(2.0, 5), 
 						 new Ride(0.1, 1)
 						};
-		double fare = invoiceGenerator.calculateFare(rides);
+		double fare = invoiceService.calculateFare(rides);
 		Assert.assertEquals(30, fare, 0.0);
 	}
 }
