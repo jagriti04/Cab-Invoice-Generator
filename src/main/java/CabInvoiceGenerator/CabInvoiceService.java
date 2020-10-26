c5d0741a9078ce4b17a9e58e36b5371ac4ff0483
Jagriti <jagriti.agni@gmail.com>
2020-10-26 16:08:50 +0530

    [Jagriti]ADDED: Ride repository class and methods to add rides by userId using HashMap

--- src/main/java/CabInvoiceGenerator/CabInvoiceService.java
+++ src/main/java/CabInvoiceGenerator/CabInvoiceService.java
@@ -4,17 +4,31 @@ public class CabInvoiceService {
 	private static final double MINIMUM_COST_PER_KM = 10;
 	private static final int MIN_COST_PER_TIME = 1;
 	private static final double MIN_TOTAL_FARE = 5;
+	private RideRepository rideRepository;
+
+	public CabInvoiceService() {
+		this.rideRepository = new RideRepository();
+	}
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = (distance * MINIMUM_COST_PER_KM) + (time * MIN_COST_PER_TIME);
 		return Math.max(totalFare, MIN_TOTAL_FARE);
 	}
-	
+
 	public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for (Ride ride : rides) {
 			totalFare = totalFare + calculateFare(ride.getDistance(), ride.getTime());
-		};
+		}
+		;
 		return new InvoiceSummary(rides.length, totalFare);
+	}
+	
+	public void addRides(String userId, Ride[] rides) {
+		rideRepository.addRides(userId, rides);;
+	}
+	
+	public InvoiceSummary getInvoiceSummary(String userId) {
+		return this.calculateFare(rideRepository.getRides(userId));
 	}
 }
---
+++ src/main/java/CabInvoiceGenerator/RideRepository.java
@@ -0,0 +1,26 @@
+package CabInvoiceGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RideRepository {
+	Map<String, ArrayList<Ride>> userRides = null;
+
+	public RideRepository() {
+		userRides = new HashMap<>();
+	}
+
+	public void addRides(String userId, Ride[] rides) {
+		ArrayList<Ride> rideList = this.userRides.get(userId);
+		if (rideList == null) {
+			rideList = new ArrayList<>(Arrays.asList(rides));
+			this.userRides.put(userId, rideList);
+		}
+	}
+
+	public Ride[] getRides(String userId) {
+		return this.userRides.get(userId).toArray(new Ride[0]);
+	}
+}
