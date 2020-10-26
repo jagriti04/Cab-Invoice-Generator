d3eb03f95f09b01002e084ee2fbaa711b75f521c
Jagriti <jagriti.agni@gmail.com>
2020-10-26 13:35:31 +0530

    [Jagriti]ADDED: Method in CabInvoiceGenerator class to calculate total fare

--- src/main/java/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -1,7 +1,12 @@
 package CabInvoiceGenerator;
 
 public class CabInvoiceGenerator {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the cab invoice generator");
+	private static final double MINIMUM_COST_PER_KM = 10;
+	private static final int MIN_COST_PER_TIME = 1;
+	private static final double MIN_TOTAL_FARE = 5;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = (distance * MINIMUM_COST_PER_KM) + (time * MIN_COST_PER_TIME);
+		return Math.max(totalFare, MIN_TOTAL_FARE);
 	}
 }
